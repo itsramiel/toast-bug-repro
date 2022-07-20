9026370bedf0a01e75925e48fd2703555443e112
Rami Elwan <ramielwan48@gmail.com>
2022-07-20 14:11:42 +0300

    example app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StatusBar, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { ToastProvider, useToast } from "react-native-toast-notifications";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ToastProvider>
+      <View style={styles.container}>
+        <Component />
+        <TouchableOpacity onPress={() => Alert.alert("pressed")} style={[styles.touchable, { marginTop: 10 }]}>
+          <Text style={{ color: "#fff", textAlign: "center" }}>Press me if you can</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => Alert.alert("pressed")} style={styles.touchable}>
+          <Text style={{ color: "#fff", textAlign: "center" }}>Press me if you can</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => Alert.alert("pressed")} style={[styles.touchable, { marginBottom: 10 }]}>
+          <Text style={{ color: "#fff", textAlign: "center" }}>Press me if you can</Text>
+        </TouchableOpacity>
+      </View>
+    </ToastProvider>
   );
 }
+
+const Component = () => {
+  const toast = useToast();
+
+  return (
+    <View style={[StyleSheet.absoluteFillObject, { top: 100 }]}>
+      <Button title="bottom" onPress={() => toast.show("I hide touchables behind me", { placement: "bottom" })} />
+      <Button title="center" onPress={() => toast.show("I hide touchables behind me", { placement: "center" })} />
+      <Button title="top" onPress={() => toast.show("I hide touchables behind me", { placement: "top" })} />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  touchable: {
+    backgroundColor: "tomato",
+    padding: 10,
+    width: "100%",
   },
 });
